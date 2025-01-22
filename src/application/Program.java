a64238e37af507d18ab9b9101c9c2921fd3e7cd5
RaphaeldosSantosSouza <164585988+RaphaeldosSantosSouza@users.noreply.github.com>
2025-01-22 10:24:53 -0300

    Update Implemention

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Robisson", "rob@g.com", new Date(), 3000.0,department );
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + +newSeller.getId());
+		
+		System.out.println("\n=== TESTE 5: Seller insert ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Ana");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
